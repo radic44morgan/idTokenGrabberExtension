e82629d08e7fc5139b0c1a50069fc68f75d8f0db
Morgan Radic <radic44morgan@gmail.com>
2020-07-20 19:26:10 -0400

    adding popup to copy ID from . still need to actualy copy to clipboard:

--- popup.js
+++ popup.js
@@ -1,10 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('grabIdToken');
-    checkPageButton.addEventListener('click', function() {
+    checkPageButton.addEventListener('click', async function() {
+        // Get the current tab
+        const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+        const tab = tabs[0]; 
+
+        // Execute script in the current tab
+        const key = 'CognitoIdentityServiceProvider.6ah99cfssnhfikn6oe5b5b3o0k.3c155f5f-2d88-4318-a834-55676870aad4.idToken'
+        const fromPageLocalStore = await chrome.tabs.executeScript(tab.id, { code: `localStorage['${key}']` });
+
         var idToken = document.createElement("textarea");
-        idToken.value = localStorage.getItem('id_token')
+        idToken.value = fromPageLocalStore
+        alert(fromPageLocalStore)
         idToken.select();
         document.execCommand('copy')
         document.body.removeChild(idToken);
     }, false);
   }, false);
+  
